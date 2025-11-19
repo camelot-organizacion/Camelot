f6d533e6c7e3561418c06b69d25e3653e7308f51
Diego <hernandezr.diego.p8@gmail.com>
2025-11-19 09:37:43 -0600

    Configuracion login y registro

---
+++ src/main/java/com/example/clientes_venta/Login/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.clientes_venta.Login;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity
+            .formLogin(httpForm ->
+                    {httpForm
+                        .loginPage("/login").permitAll();
+                    }).build();
+    }
+}
